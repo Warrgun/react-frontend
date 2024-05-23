c17c8d7b5c1e3dc0b4f41c40f8c0f9052c03e082
Warrgun <131681381+Warrgun@users.noreply.github.com>
2024-05-23 19:40:47 +0200

    new cards

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,10 @@
 import Card from "./Card"
-
+import profilePic from "./assets/profile.jpg"
 
 function App() {
     return(
       <>
-        <Card/>
+        <Card picture = {profilePic} name="Fabio" age ={20} isSingle = {true}/>
         <Card/>
         <Card/>
         <Card/>
--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,16 +1,30 @@
-import profilePic from "./assets/profile.jpg"
 import Button from "./button/Button.jsx"
+import PropTypes from "prop-types"
 
-function Card(){
+function Card({
+    picture = "https://placehold.co/400", 
+    name = "Guest", 
+    age = 0,
+    isSingle = false
+}){
 
     return(
         <div className="card">
-            <img className="card-img" src={profilePic} alt="Profile picture"></img>
-            <h2 className="card-title">Fabio</h2>
-            <p className="card-p">Just here to learn react</p>
+            <img className="card-img" src={picture} alt="Profile picture"></img>
+            <h2 className="card-title">{name}, {age}</h2>
+            <p className="card-p">{isSingle ? "Wolny":"Zajęty"}</p>
             <Button/>
         </div>
     );
 }
+
+Card.proptypes ={
+    picture: PropTypes.string,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isSingle: PropTypes.bool,
+}
+
+
 
 export default Card

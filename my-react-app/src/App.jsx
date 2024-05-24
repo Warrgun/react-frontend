44e7ffe456d80c50bc0ff6c5a713fb71e0c80d11
Warrgun <131681381+Warrgun@users.noreply.github.com>
2024-05-24 22:32:44 +0200

    k

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,13 +4,22 @@ import profilePic from "./assets/profile.jpg"
 
 
 function App() {
+
+    const fruits = [
+    {id: 1, name: "Jabłuko", prize: 2.99},
+    {id: 2, name: "Gruszka", prize: 3.99},
+    {id: 3, name: "Pomarańcza", prize: 2.05},
+    {id: 4, name: "Pomidor", prize: 1.50}
+    ];
+  
+    
     return(
       <>
         <Card picture = {profilePic} name="Fabio" age ={20} isSingle = {true}/>
         <Card/>
         <Card/>
         <Card/>
-        <List />
+        {fruits.length > 0 && <List items={fruits} category="Fruits"/>}
       </>
     );
 }
--- my-react-app/src/ListFruits.jsx
+++ my-react-app/src/ListFruits.jsx
@@ -1,25 +1,54 @@
+import PropTypes from "prop-types"
 
+function List({
+    category = "Category",
+    items = []
+}){
+    const categoryStyle = {
+        fontSize: "2.5em",
+        fontWeight: "bold",
+        border: "2px solid",
+        borderRadius: "5px",
+        textAlign: "center",
+        padding: "10px",
+        marginBottom: "20px",
+        backgroundColor: "hsl(0, 96%, 82%)"
+    }
 
-function List(){
-    const fruits = [
-        {id: 1, name: "Jabłuko", prize: 2.99},
-        {id: 2, name: "Gruszka", prize: 3.99},
-        {id: 3, name: "Pomarańcza", prize: 2.05},
-        {id: 4, name: "Pomidor", prize: 1.50}
-    ];
-
+    const listStyle = {
+        fontSize: "1.5em",
+        listStyle: "none",
+        color: "hsl(0%,0%,25%)",
+        textAlign: "center",
+        margin: "0"
+    }
+    
     //fruits.sort((a, b) => a.name.localeCompare(b.name)); alfabetucznie
     //fruits.sort((a, b) => a.prize - b.prize); numerycznie
 
-    const listItems = fruits.map( fruit => <li key = {fruit.id}>
-                                                    {fruit.name}: &nbsp;
-                                                    <b>{fruit.prize}</b></li>);
+    const listItems = items.map( item => <li key = {item.id}>
+                                                    {item.name}: &nbsp;
+                                                    <b>{item.prize}</b></li>);
                                                     
     return(
-        <ol>
-            {listItems}
-        </ol>
+        <>
+            <h1 className="category" style={categoryStyle}>{category}</h1>
+            <ol className="items-list" style={listStyle}>
+                {listItems}
+            </ol>
+        </>
+        
     );
+}
+
+List.propTypes = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        prize: PropTypes.number
+    }))
+
 }
 
 export default List
